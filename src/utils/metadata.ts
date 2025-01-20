1c74433bb9365778902af0931425b9c98cc10ffd
21 Tech Enterprise <59superrichsol@gmail.com>
2025-01-20 19:55:39 +0700

    fix matadata

--- src/utils/metadata.ts
+++ src/utils/metadata.ts
@@ -39,7 +39,6 @@ export const generateMetadata = ({
         "multilingual marketing",
         "Gold Trading",
         "marketing workflow",
-     
     ],
     author = process.env.NEXT_PUBLIC_AUTHOR_NAME,
     type = "website",
@@ -57,11 +56,43 @@ export const generateMetadata = ({
         authors: [{ name: author }],
         creator: author,
         publisher: process.env.NEXT_PUBLIC_APP_NAME,
+        robots: {
+            index: !noIndex,
+            follow: !noIndex,
+            googleBot: {
+                index: !noIndex,
+                follow: !noIndex,
+            },
+        },
+        openGraph: {
+            type,
+            title,
+            description,
+            siteName: process.env.NEXT_PUBLIC_APP_NAME,
+            url: metadataBase,
+            images: [
+                {
+                    url: "/images/dashboard_Aurienn.png",
+                    width: 1200,
+                    height: 630,
+                    alt: title,
+                },
+            ],
+        },
+        twitter: {
+            card: "summary_large_image",
+            title,
+            description,
+            images: ["/images/dashboard_Aurienn.png"],
+        },
         formatDetection: {
             email: false,
             address: false,
             telephone: false,
         },
         icons,
+        alternates: {
+            canonical: metadataBase.toString(),
+        },
     };
 };
