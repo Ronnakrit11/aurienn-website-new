cd289ce62370751d2dec0ed8cfe5aeb181f86612
21 Tech Enterprise <59superrichsol@gmail.com>
2025-01-28 17:39:36 +0700

    done

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,20 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
+  },
+  headers: async () => {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'X-Robots-Tag',
+            value: 'index, follow'
+          }
+        ]
+      }
+    ];
   }
 };
 
 export default nextConfig;
-
-
