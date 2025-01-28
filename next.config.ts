18bd187695587d082a89aa1b074f45d27e5c0ceb
21 Tech Enterprise <59superrichsol@gmail.com>
2025-01-28 17:50:59 +0700

    done

--- next.config.ts
+++ next.config.ts
@@ -11,7 +11,7 @@ const nextConfig: NextConfig = {
         headers: [
           {
             key: 'X-Robots-Tag',
-            value: 'index, follow'
+            value: 'all'
           }
         ]
       }
--- src/app/robots.ts
+++ src/app/robots.ts
@@ -2,11 +2,14 @@ import { MetadataRoute } from 'next'
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-     
-    },
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: ['/api/*', '/admin/*']
+      }
+    ],
     sitemap: 'https://www.aurienn.com/sitemap.xml',
+    host: 'https://www.aurienn.com'
   }
 }
