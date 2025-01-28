a77096551677be4221d2c1e71d0483b8afcea5de
21 Tech Enterprise <59superrichsol@gmail.com>
2025-01-28 17:18:54 +0700

    done

--- next.config.ts
+++ next.config.ts
@@ -8,20 +8,4 @@ const nextConfig: NextConfig = {
 
 export default nextConfig;
 
-module.exports = {
-  async headers() {
-    const headers = [];
-    if (process.env.NEXT_PUBLIC_VERCEL_ENV !== 'production') {
-      headers.push({
-        headers: [
-          {
-            key: 'X-Robots-Tag',
-            value: 'noindex',
-          },
-        ],
-        source: '/:path*',
-      });
-    }
-    return headers;
-  },
- };
+
